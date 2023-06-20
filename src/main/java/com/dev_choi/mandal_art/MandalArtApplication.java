770ddcd89dbd108c83d0a6f1bf6b31045f12a013
myeongbaek <moungbak421@daum.net>
2023-06-20 15:50:35 +0900

    initial : Mandal Art Project java : 11 springboot : 2.7.7 dependency : JPA, Thymeleaf, SpringWeb, Lombok, H2, MySQL

---
+++ src/main/java/com/dev_choi/mandal_art/MandalArtApplication.java
@@ -0,0 +1,13 @@
+package com.dev_choi.mandal_art;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MandalArtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MandalArtApplication.class, args);
+    }
+
+}

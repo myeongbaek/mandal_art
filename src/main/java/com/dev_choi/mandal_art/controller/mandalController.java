f558e7f2c6cebacb019e4fea1004b66ec6faa93b
myeongbaek <moungbak421@daum.net>
2023-06-29 04:07:28 +0900

    feature :  PostMapping 구현
    - 프론트 : ES6 fetch()를 활용해 POST 요청을 동작한다.
    - 서버 : @RestController로 요청을 응답한다.

---
+++ src/main/java/com/dev_choi/mandal_art/controller/mandalController.java
@@ -0,0 +1,32 @@
+package com.dev_choi.mandal_art.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class mandalController {
+    @PostMapping(value = "/mandal")
+    public Map mandalSave(@RequestBody Map<String, Object> requestData) {
+        Map<String, Object> result = new HashMap();
+
+        requestData.forEach((key, value) -> {
+            System.out.println("key : " + key);
+            System.out.println("value : " + value);
+        });
+
+        result.put("msg", "Success");
+        return result;
+    }
+
+    @PostMapping(value = "/mandal/sub")
+    public void mandalSubSave(@RequestBody Map<String, Object> requestData) {
+        requestData.forEach((key, value) -> {
+            System.out.println("key : " + key);
+            System.out.println("value : " + value);
+        });
+    }
+}
